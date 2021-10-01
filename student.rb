68101f6dbd9f97bdca8782934b482215adf2c358
Usman Suleiman <usmansbk@gmail.com>
2021-10-01 21:31:11 +0100

    Create studen class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require_relative 'person'
+
+class Student < Person
+  def initialize(:name = "Unknown", :age, :parent_permission = true, :classroom)
+    super(:name, :age, :parent_permission)
+    @classroom = :classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+  
