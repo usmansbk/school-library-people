48911ca75dd0085b1fa4aa4e7b81acd9da0e933d
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 10:15:55 +0100

    Prompt create person

--- main.rb
+++ main.rb
@@ -8,6 +8,26 @@ class App
   end
 
   def create_person_type
+    print "Do you want to create a student (1) or a teacher (2)? [Input the number]:"
+    option = gets.chomp
+
+    case option
+    when '1'
+      print 'Age: '
+      age = gets.chomp
+
+      print 'Name: '
+      name = gets.chomp
+
+      print 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp.downcase
+
+      puts "Person created successfully"
+    when '2'
+      puts 'teacher'
+    else
+      puts 'That is not a valid input'
+    end
   end
 
   def create_book
