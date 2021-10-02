eb29c516293a53d2e62846463ff76cb17138d232
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 12:57:07 +0100

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,18 +1,19 @@
-require_relative "corrector"
+require_relative 'corrector'
 
 class Person
   attr_accessor :name, :age
   attr_reader :id, :rentals
 
-  def initialize(name: "Unknown", age:, parent_permission: true)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
     @rentals = []
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   def validate_name
@@ -22,7 +23,7 @@ class Person
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
--- student.rb
+++ student.rb
@@ -1,9 +1,9 @@
-require_relative "person"
+require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name: "Unknown", age:, parent_permission: true, classroom:)
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
@@ -17,4 +17,3 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 end
-  
