3cd028e49e2f400d56b84ddb005bc2735be1e432
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 13:00:57 +0100

    Fix linter error

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
-require_relative "person"
+require_relative 'person'
 
 class Student < Person
-  def initialize(name: "Unknown", age:, parent_permission: true, classroom:)
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
@@ -10,4 +10,3 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 end
-  
