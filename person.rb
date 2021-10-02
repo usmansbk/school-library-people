6bf0971f40f19ccb39a6fa9b65af8e496c6c7ef1
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 08:41:05 +0100

    Create many-to-many relationship

--- person.rb
+++ person.rb
@@ -2,12 +2,13 @@ require_relative 'corrector'
 
 class Person
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(name: "Unknown", age:, parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @rentals = []
   end
 
   def can_use_services?
