5be1ee45fb24ac84cca5cbc495717735225ebce1
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 07:02:30 +0100

    Fix keyword argument

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,9 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(:name = "Unknown", :age, :parent_permission = true, :specialization)
-    super(:name, :age, :parent_permission)
-    @specialization = :specialization
+  def initialize(name: = "Unknown", age:, parent_permission: = true, specialization:)
+    super(name, age, parent_permission)
+    @specialization = specialization
   end
 
   def can_use_services?
