ff0db7ad2a917e818b5c9da16160d55f97331e22
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 09:20:30 +0100

    Make executable

--- main.rb
+++ main.rb
@@ -1,22 +1,32 @@
-def list_books
-end
+#!/usr/bin/env ruby
 
-def list_people
-end
+class App
+  def list_books
+  end
 
-def create_person_type
-end
+  def list_people
+  end
 
-def create_book
-end
+  def create_person_type
+  end
 
-def create_rental
-end
+  def create_book
+  end
 
-def list_rentals_by_person_id
+  def create_rental
+  end
+
+  def list_rentals_by_person_id
+  end
+
+  def print_welcome
+    puts
+  end
 end
 
 def main
+  app = App.new
+  app.run
 end
 
 main
