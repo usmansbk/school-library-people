7fb8dcad1d3be2cccdce88f6058de240510a3aa8
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 10:18:55 +0100

    Initialize

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
+require_relative 'person'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'book'
+require_relative 'rental'
+
 class App
   def list_books
   end
