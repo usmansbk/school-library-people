108cfb077c089340a47b8696ab3c065e7c93f19d
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 07:54:22 +0100

    Fix corrector

--- person.rb
+++ person.rb
@@ -8,7 +8,6 @@ class Person
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-    @corrector = Corrector.new
   end
 
   def can_use_services?
@@ -16,7 +15,8 @@ class Person
   end
 
   def validate_name
-    @name = @corrector.correct_name @name
+    corrector = Corrector.new
+    @name = corrector.correct_name @name
   end
 
   private
