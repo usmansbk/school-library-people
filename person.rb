a659d8f80637bbe712cc4a3cad5339f3086f357e
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 07:46:15 +0100

    Use attribute reader

--- person.rb
+++ person.rb
@@ -2,16 +2,13 @@ require_relative 'corrector'
 
 class Person
   attr_accessor :name, :age
+  attr_reader :id
 
   def initialize(name: "Unknown", age:, parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @corrector = Corrector.new
-  end
-
-  def id
-    @id
   end
 
   def can_use_services?
