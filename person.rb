58d3540b7562e985e8dd7fa89c38f8343d56c602
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 07:09:31 +0100

    Fix keyword argument

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(name: = "Unknown", age:, parent_permission: = true)
+  def initialize(name: "Unknown", age:, parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(name: = "Unknown", age:, parent_permission: = true, specialization:)
+  def initialize(name: "Unknown", age:, parent_permission: true, specialization:)
     super(name, age, parent_permission)
     @specialization = specialization
   end
