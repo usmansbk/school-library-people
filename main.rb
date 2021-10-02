b0e8813c6c529b9fae44706729db5cc03877b708
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 12:49:11 +0100

    Fix linter errors

--- main.rb
+++ main.rb
@@ -19,32 +19,37 @@ class App
 
     loop do
       print_options
+
       option = gets.chomp
 
-      case option
-      when '1'
-        list_books
-      when '2'
-        list_people
-      when '3'
-        create_person
-      when '4'
-        create_book
-      when '5'
-        create_rental
-      when '6'
-        list_rentals_by_person_id
-      when '7'
-        break
-      else
-        puts 'That is not a valid option'
-      end
+      break if option == '7'
+
+      handle_option option
     end
 
     puts 'Thank you for using this app!'
   end
 
   private
+
+  def handle_option(option)
+    case option
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals_by_person_id
+    else
+      puts 'That is not a valid option'
+    end
+  end
 
   def print_options
     puts
