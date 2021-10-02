ae8b2c6eba5b58e3b72221cd73d7fbe92f189354
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 06:24:29 +0100

    Use decorator

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
+require_relative 'corrector'
+
 class Person
   def initialize(:name = "Unknown", :age, :parent_permission = true)
     @id = Random.rand(1..1000)
     @name = :name
     @age = :age
+    @corrector = Corrector.new
   end
 
   def get_id
@@ -27,6 +30,10 @@ class Person
 
   def can_use_services?
     is_of_age || @parent_permission
+  end
+
+  def validate_name
+    @name = @corrector.correct_name @name
   end
 
   private
