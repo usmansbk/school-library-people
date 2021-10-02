5c34998c7067ed1481842242c5bc6229daa06f24
Usman Suleiman <usmansbk@gmail.com>
2021-10-02 09:10:36 +0100

    Add stub methods

---
+++ main.rb
@@ -0,0 +1,22 @@
+def list_books
+end
+
+def list_people
+end
+
+def create_person_type
+end
+
+def create_book
+end
+
+def create_rental
+end
+
+def list_rentals_by_person_id
+end
+
+def main
+end
+
+main
