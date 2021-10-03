5fbb38b440d26797bb01cdf01c7ba64c57335756
Usman Suleiman <usmansbk@gmail.com>
2021-10-03 10:44:34 +0100

    Fix linter errors

--- persistor.rb
+++ persistor.rb
@@ -16,52 +16,63 @@ class Persistor
   def parse_books
     file = 'books.json'
 
-    if !File.exist? file
-      []
-    else
+    if File.exist? file
       JSON.parse(File.read(file), create_additions: true)
+    else
+      []
     end
   end
 
   def parse_rentals(people, books)
     file = 'rentals.json'
 
-    if !File.exist? file
-      []
-    else
+    if File.exist? file
       JSON.parse(File.read(file)).map do |rental_json|
         book = books.find { |current_book| current_book.title == rental_json['book_title'] }
         person = people.find { |current_person| current_person.id == rental_json['person_id'].to_i }
 
         Rental.new(rental_json['date'], book, person)
       end
+    else
+      []
     end
   end
 
   def parse_people(classroom)
     file = 'people.json'
 
-    if !File.exist? file
-      []
-    else
+    if File.exist? file
       JSON.parse(File.read(file)).map do |person_json|
-        id = person_json['id'].to_i
-        name = person_json['name']
-        age = person_json['age']
-
         if person_json['json_class'] == 'Student'
-          parent_permission = person_json['parent_permission']
-          student = Student.new(name: name, age: age, parent_permission: parent_permission, classroom: classroom)
-          student.id = id
-          student
+          create_student(person_json, classroom)
         else
-          specialization = person_json['specialization']
-          teacher = Teacher.new(name: name, age: age, specialization: specialization)
-          teacher.id = id
-          teacher
+          create_teacher person_json
         end
       end
+    else
+      []
     end
+  end
+
+  def create_teacher(person_json)
+    id = person_json['id'].to_i
+    name = person_json['name']
+    age = person_json['age']
+    specialization = person_json['specialization']
+    teacher = Teacher.new(name: name, age: age, specialization: specialization)
+    teacher.id = id
+    teacher
+  end
+
+  def create_student(person_json, classroom)
+    id = person_json['id'].to_i
+    name = person_json['name']
+    age = person_json['age']
+    parent_permission = person_json['parent_permission']
+
+    student = Student.new(name: name, age: age, parent_permission: parent_permission, classroom: classroom)
+    student.id = id
+    student
   end
 
   def persist(people:, rentals:, books:)
