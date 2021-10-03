b504bf4b665a92f5b72fb9a9e6ed92b192c78dc1
Usman Suleiman <usmansbk@gmail.com>
2021-10-03 17:05:38 +0100

    Test book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,12 +2,30 @@ require_relative 'spec_helper'
 
 describe Book do
   before :each do
-    @book = Book.new("Lord of the rings", "Tolkien")
+    @book = Book.new('Lord of the rings', 'Tolkien')
   end
 
-  describe "#new" do
-    it "takes two parameters and returns a Book object" do
-      @book.should be_an_instance_of Book
+  describe '#new' do
+    it 'takes two parameters and returns a Book object' do
+      expect(@book).to be_an_instance_of Book
+    end
+  end
+
+  describe '#rentals' do
+    it "returns the list of rentals" do
+      expect(@book.rentals).to eq []
+    end
+  end
+
+  describe '#author' do
+    it 'returns the correct author' do
+      expect(@book.author).to eq 'Tolkien'
+    end
+  end
+
+  describe '#title' do
+    it 'returns the correct title' do
+      expect(@book.title).to eq 'Lord of the rings'
     end
   end
 end
