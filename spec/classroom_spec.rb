80a3f98f80a83796e7eb1c71c4020635f8270eb8
Usman Suleiman <usmansbk@gmail.com>
2021-10-11 14:41:05 +0100

    Test add student

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -3,6 +3,8 @@ require_relative 'spec_helper'
 describe Classroom do
   before :each do
     @classroom = Classroom.new('Microverse')
+    @student = Student.new(age: 17, classroom: @classroom)
+    @classroom.add_student @student
   end
 
   describe '#new' do
@@ -13,13 +15,27 @@ describe Classroom do
 
   describe '#students' do
     it 'returns a has-many Student relationship' do
-      expect(@classroom).to have_attributes('students' => [])
+      expect(@classroom).to have_attributes('students' => [@student])
     end
   end
 
   describe '#label' do
     it 'returns the correct label' do
       expect(@classroom.label).to eq 'Microverse'
+    end
+  end
+
+  describe '#add_student' do
+    context 'classroom' do
+      it 'should add a student' do
+        expect(@classroom.students).to include(@student)
+      end
+    end
+
+    context 'student' do
+      it 'should be in the correct classroom' do
+        expect(@student.classroom).to eq @classroom
+      end
     end
   end
 end
